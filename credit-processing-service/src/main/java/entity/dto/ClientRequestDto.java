d6a7f78d73bb78a3aa25054dbead95af3c49c831
Jb192101 <grigorijbaryshev910@gmail.com>
2025-09-21 21:39:49 +0300

    Добавление enum-ам стринговых значений

--- credit-processing-service/src/main/java/entity/dto/ClientRequestDto.java
+++
@@ -1,8 +0,0 @@
-package entity.dto;
-
-public class ClientRequestDto {
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private String documentId;
-}
